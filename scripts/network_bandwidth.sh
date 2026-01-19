4f5cb45cd7c995f85752310a66cdad7291abb2db
Yuan Xu <neuromoments@gmail.com>
2026-01-19 10:48:39 +0800

    Refactor network bandwidth script for improved clarity

--- scripts/network_bandwidth.sh
+++ scripts/network_bandwidth.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# INTERVAL is equal to 1s because we want to express the bandwidth in sec
-readonly INTERVAL=1
-
 # UPLOAD and DOWNLOAD index
 readonly UPLOAD=0
 readonly DOWNLOAD=1
@@ -10,9 +7,9 @@ readonly DOWNLOAD=1
 # SIZE index are the multiple of the unit byte and value the internationally recommended unit symbol in sec
 readonly SIZE=(
   [1]='B/s'
-  [1024]='kB/s'
-  [1048576]='MB/s'
-  [1073741824]='GB/s'
+  [1000]='kB/s'
+  [1000000]='MB/s'
+  [1000000000]='GB/s'
 )
 
 # interface_get try to automaticaly get the used interface if network_name is empty
@@ -23,7 +20,7 @@ interface_get() {
     case "$(uname -s)" in
     Linux)
       if type ip >/dev/null; then
-        name="$(ip -o route get 192.168.0.0 | awk '{print $5}')"
+        name="$(ip -o route show default | awk '{print $5; exit}')"
       fi
       ;;
     Darwin)
@@ -41,10 +38,10 @@ interface_get() {
 interface_bytes() {
   case "$(uname -s)" in
   Linux)
-    upload=$(cat "/sys/class/net/$1/statistics/tx_bytes")
-    download=$(cat "/sys/class/net/$1/statistics/rx_bytes")
+    upload_rate=$(cat "/sys/class/net/$1/statistics/tx_bytes")
+    download_rate=$(cat "/sys/class/net/$1/statistics/rx_bytes")
 
-    echo "$upload $download"
+    echo "$upload_rate $download_rate"
     ;;
   Darwin)
     # column 7 is Ibytes (in bytes, rx, download) and column 10 is Obytes (out bytes, tx, upload)
@@ -55,38 +52,46 @@ interface_bytes() {
 
 # get_bandwidth return the number of bytes exchanged for tx and rx
 get_bandwidth() {
-  local upload=0
-  local download=0
+  local interface="$1"
+  local interval="$2"
 
-  IFS=' ' read -r upload download <<< "$(interface_bytes "$1")"
-
-  # wait for interval to calculate the difference
-  sleep "$INTERVAL"
-
-  IFS=' ' read -r new_upload new_download <<< "$(interface_bytes "$1")"
-
-  upload=$(( $new_upload - $upload ))
-  download=$(( $new_download - $download ))
+  IFS=' ' read -r old_upload old_download <<< "$(interface_bytes "$interface")"
+  sleep "$interval"
+  IFS=' ' read -r new_upload new_download <<< "$(interface_bytes "$interface")"
+  upload_rate=$(( (new_upload - old_upload) / interval ))
+  download_rate=$(( (new_download - old_download) / interval ))
 
   # set to 0 by default
-  echo "${upload:-0} ${download:-0}"
+  echo "${upload_rate:-0} ${download_rate:-0}"
 }
 
-# bandwidth_to_unit convert bytes into its highest unit and add unit symbol in sec
 bandwidth_to_unit() {
   local size=1
   for i in "${!SIZE[@]}"; do
     if (($1 < i)); then
       break
     fi
-
     size="$i"
   done
 
-  local result="0.00"
-  if (($1 != 0)); then
-    result="$(awk -v a="$1" -v b="$size" 'BEGIN { printf "%.2f\n", a / b }' </dev/null)"
-  fi
+  local result
+  # Use awk to format the value to 3 significant figures
+  result=$(awk -v bytes="$1" -v divisor="$size" '
+    BEGIN {
+      if (bytes == 0) {
+        printf "0.00";
+        exit;
+      }
+      val = bytes / divisor;
+      if (val < 10) {
+        fmt = "%.2f";
+      } else if (val < 100) {
+        fmt = "%.1f";
+      } else {
+        fmt = "%.0f";
+      }
+      printf(fmt, val);
+    }' </dev/null)
 
   echo "$result ${SIZE[$size]}"
 }
@@ -97,11 +102,13 @@ main() {
 
   network_name=""
   show_interface="$(tmux show-option -gqv "@dracula-network-bandwidth-show-interface")"
-  interval_update="$(tmux show-option -gqv "@dracula-network-bandwidth-interval")"
+  interval="$(tmux show-option -gqv "@dracula-network-bandwidth-interval")"
 
   if [[ -z $interval_update ]]; then
     interval_update=0
   fi
+
+  printf "%4s%4s↓ %4s%4s↑\n" - B/s - B/s
 
   while true; do
     if ((counter == 0)); then
@@ -109,13 +116,12 @@ main() {
       network_name="$(interface_get)"
     fi
 
-    IFS=" " read -ra bandwidth <<<"$(get_bandwidth "$network_name")"
+    IFS=" " read -ra bandwidth <<<"$(get_bandwidth "$network_name" "$interval")"
 
     if [[ $show_interface == "true" ]]; then echo -n "[$network_name] "; fi
-    printf "↓ %6s %-4s • ↑ %6s %-4s\n" $(bandwidth_to_unit "${bandwidth[$DOWNLOAD]}") $(bandwidth_to_unit "${bandwidth[$UPLOAD]}")
+    printf "%4s%4s↓ %4s%4s↑\n" $(bandwidth_to_unit "${bandwidth[$DOWNLOAD]}") $(bandwidth_to_unit "${bandwidth[$UPLOAD]}")
 
     ((counter = counter - 1))
-    sleep "$interval_update"
   done
 }
 
