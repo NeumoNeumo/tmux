d7bf26ad5f905fc3cc7d0366be5f4a2a1c3c874a
Jianhui Xu <neuromoments@gmail.com>
2026-07-01 23:04:57 +0800

    update

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -167,10 +167,10 @@ main() {
   tmux set-option -g pane-border-style "fg=${gray}"
 
   # message styling
-  tmux set-option -g message-style "bg=${gray},fg=${white}"
+  tmux set-option -g message-style "bg=${gray},fg=${white},fill=${gray},width=100%,align=left"
 
   # status bar
-  tmux set-option -g status-style "bg=${bg_color},fg=${white}"
+  tmux set-option -g status-style "bg=${gray},fg=${white},fill=${gray},width=100%,align=left"
 
   # Status left
   if $show_powerline; then
